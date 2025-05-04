66e359eaf80e25af09235814327902ca7355789a
VAJID ALI <sameervajidali@gmail.com>
2025-05-04 22:40:25 +0530

    A U Page Design

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    cors: true,
+    origin: "http://localhost:5173",
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
-        // strip the domain attribute so cookie ends up on your dev server
         cookieDomainRewrite: {
-          // rewrite `Domain=localhost;` → no Domain (i.e. current host:5173)
-          "localhost": ""
+          // Ensure cookies are set for localhost:5173
+          "localhost": "",
         },
       },
     },
