2faf103bf6c1d665d20bb08534d15ace46465447
VAJID ALI <sameervajidali@gmail.com>
2025-05-05 12:54:11 +0530

    Backend URL Chnage

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-// frontend/vite.config.js
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    cors: true,
-    origin: "http://localhost:5173",
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-        cookieDomainRewrite: {
-          // Ensure cookies are set for localhost:5173
-          "localhost": "",
-        },
-      },
-    },
-  },
 })
