1449afd31e3672b43d0a50b2e65a281bf5c779ba
VAJID ALI <sameervajidali@gmail.com>
2025-05-07 17:22:21 +0530

    Quiz Module Testing

--- src/pages/admin/Quiz/QuestionForm.jsx
+++ src/pages/admin/Quiz/QuestionForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import questionService from '../../services/questionService.js';
+import questionService from '../../../services/questionService.js';
 
 export default function QuestionForm() {
   const { quizId, questionId } = useParams();
