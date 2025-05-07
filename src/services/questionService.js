11b6d527ccd8a0401bc3042e5316a6a4ffea728f
VAJID ALI <sameervajidali@gmail.com>
2025-05-07 17:25:56 +0530

    Quiz Module Testing

---
+++ src/services/questionService.js
@@ -0,0 +1,31 @@
+import API from './axios.js';
+
+export function getQuestionsByQuizId(quizId) {
+  return API.get(`/quizzes/admin/quizzes/${quizId}/questions`)
+            .then(res => res.data);
+}
+
+export function createQuestion(quizId, payload) {
+  return API.post(`/quizzes/admin/quizzes/${quizId}/questions`, payload)
+            .then(res => res.data);
+}
+
+export function updateQuestion(quizId, questionId, payload) {
+  return API.patch(
+    `/quizzes/admin/quizzes/${quizId}/questions/${questionId}`,
+    payload
+  ).then(res => res.data);
+}
+
+export function deleteQuestion(quizId, questionId) {
+  return API.delete(
+    `/quizzes/admin/quizzes/${quizId}/questions/${questionId}`
+  ).then(res => res.data);
+}
+
+export default {
+  getQuestionsByQuizId,
+  createQuestion,
+  updateQuestion,
+  deleteQuestion,
+};
