7c5878ab37827d7fe4dc439e83f5d8228616d225
VAJID ALI <sameervajidali@gmail.com>
2025-05-08 08:03:22 +0530

    Quiz Module int

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,26 @@
-
 // src/main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Toaster } from 'react-hot-toast'
-import { AuthProvider } from './contexts/AuthContext.jsx'
-import { ThemeProvider } from './contexts/ThemeContext'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ToastProvider } from "./contexts/ToastContext.jsx";
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <ThemeProvider>
       <AuthProvider>
-      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-  <App />
-</GoogleOAuthProvider>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <ToastProvider>
+            <App />
+            </ToastProvider>
+        </GoogleOAuthProvider>
         <Toaster position="top-right" />
       </AuthProvider>
     </ThemeProvider>
   </StrictMode>
-)
-
+);
