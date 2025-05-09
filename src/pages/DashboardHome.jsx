781be9b06d76dc6ad6d45345a7fbadcf006fd1e7
VAJID ALI <sameervajidali@gmail.com>
2025-05-10 00:33:39 +0530

    Admin Dashboard

--- src/pages/DashboardHome.jsx
+++ src/pages/DashboardHome.jsx
@@ -1,27 +1,19 @@
 // src/pages/DashboardHome.jsx
-
 import React from "react";
 import WelcomePanel from "../components/dashboard/WelcomePanel";
-import StatsCards from "../components/dashboard/StatsCards"; // your 4-cards row
+import StatsCards    from "../components/dashboard/StatsCards";
 import CareerSnapshot from "../components/dashboard/CareerSnapshot";
 
 export default function DashboardHome() {
   return (
     <div className="flex flex-col">
-      {/* 1) Constrain width & horizontal padding */}
       <div className="max-w-7xl mx-auto w-full px-4 sm:px-6 lg:px-8">
-        {/* 2) Top margin to push below topbar */}
-        <div className="pt-6">
-          {/* Welcome and profile completion */}
+        <div className="pt-6 space-y-8">
           <WelcomePanel />
-          {/* Spacing to next row */}
-          <div className="mt-8">
-            <StatsCards />
-          </div>
+          <StatsCards />
         </div>
       </div>
 
-      {/* 3) Career snapshot should also live inside the same container */}
       <div className="max-w-7xl mx-auto w-full px-4 sm:px-6 lg:px-8 mt-8">
         <CareerSnapshot />
       </div>
