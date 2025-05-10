3657aa3830c330665fbba30d9c48d29e87a6aebe
VAJID ALI <sameervajidali@gmail.com>
2025-05-10 20:49:11 +0530

    Package.json

--- vite.config.js
+++ vite.config.js
@@ -22,11 +22,12 @@
 // })
 
 
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
-const path = require('path');
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
@@ -34,4 +35,3 @@ module.exports = defineConfig({
     },
   },
 });
-
