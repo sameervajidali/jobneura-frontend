88603da6edd5a9419a8045301cb09628d6d64b68
VAJID ALI <sameervajidali@gmail.com>
2025-05-14 12:58:20 +0530

    New Module Admin

--- src/services/jobService.js
+++ src/services/jobService.js
@@ -1,13 +1,36 @@
-import API from './axios';
+import API from "./axios";
 
-const jobService = {
-  getAll: () => API.get('/jobs/public').then(res => res.data.jobs),
-  create: (data) => API.post('/jobs', data),
-  update: (id, data) => API.put(`/jobs/${id}`, data),
-  remove: (id) => API.delete(`/jobs/${id}`),
-  bulkUpload: (formData) => API.post('/jobs/admin/bulk-upload', formData, {
-    headers: { 'Content-Type': 'multipart/form-data' }
-  }),
+// PUBLIC: Fetch all visible jobs
+const getAllPublic = () =>
+  API.get("/jobs/public").then(res => res.data.jobs);
+
+// PUBLIC: Get a single job by ID
+const getById = (id) =>
+  API.get(`/jobs/${id}`).then(res => res.data);
+
+// ADMIN: Create a job (requires token + admin role)
+const create = (data) =>
+  API.post("/jobs/admin/jobs", data).then(res => res.data);
+
+// ADMIN: Update a job
+const update = (id, data) =>
+  API.put(`/jobs/admin/jobs/${id}`, data).then(res => res.data);
+
+// ADMIN: Delete a job
+const remove = (id) =>
+  API.delete(`/jobs/admin/jobs/${id}`).then(res => res.data);
+
+// ADMIN: Bulk upload jobs via CSV
+const bulkUpload = (formData) =>
+  API.post("/jobs/admin/jobs/bulk-upload", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  }).then(res => res.data);
+
+export default {
+  getAllPublic,
+  getById,
+  create,
+  update,
+  remove,
+  bulkUpload,
 };
-
-export default jobService;
