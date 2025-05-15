346b02bddb750600fd1a2f817816ea63cfddd3ee
VAJID ALI <sameervajidali@gmail.com>
2025-05-15 17:39:55 +0530

    Session Loading issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
   useNavigate,
 } from "react-router-dom";
 import React, { useEffect } from "react";
-import { useRef, useEffect } from 'react';
+import { useRef} from 'react';
 
 // Public Pages
 import HomePage from "./pages/HomePage";
