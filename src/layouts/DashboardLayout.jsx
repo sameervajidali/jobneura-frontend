8e9d80e6b48808bc0221c0f76f5a2355759cb404
VAJID ALI <sameervajidali@gmail.com>
2025-05-23 14:45:38 +0530

    User Profile

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -6,7 +6,7 @@ export default function DashboardLayout() {
   return (
     <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
       <Sidebar />
-     <div className="flex flex-col flex-1 min-w-0 md:ml-56">
+     <div className="flex flex-col flex-1 min-w-0 md:ml-56 border-l border-gray-200 dark:border-gray-800">
         <Topbar />
         <main className="flex-1 overflow-y-auto p-6">
           <Outlet />
