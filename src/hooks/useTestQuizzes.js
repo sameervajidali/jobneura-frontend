95a3f1187cdb0ee2b8586fca6a0f40dba9d69404
VAJID ALI <sameervajidali@gmail.com>
2025-06-07 22:04:04 +0530

    subtopic introduce

--- src/hooks/useTestQuizzes.js
+++ src/hooks/useTestQuizzes.js
@@ -6,7 +6,7 @@ export default function useTestQuizzes() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    API.get('/quizzes/test-quizzes')
+    API.get('/quizzes')
       .then(res => setData(res.data))
       .catch(err => setData({ error: err.message }));
   }, []);
