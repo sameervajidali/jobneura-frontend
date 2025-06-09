4a69a4b8f398a76d2e86358d77427ba4f6073fc2
VAJID ALI <sameervajidali@gmail.com>
2025-06-09 14:11:02 +0530

    fix: migrate react-helmet to react-helmet-async for Vercel build

--- src/pages/TutorialsPage.jsx
+++ src/pages/TutorialsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import Hero from '../components/tutorials/Hero';
 import CategoryFilter from '../components/tutorials/CategoryFilter';
 import TutorialGrid from '../components/tutorials/TutorialGrid';
